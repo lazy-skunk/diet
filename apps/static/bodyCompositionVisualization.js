986bd7ed13a46d0f16338e71d900e0b49eb415a6
hirohisahonmo <65598223+hirohisahonmo@users.noreply.github.com>
2023-09-12 22:51:45 +0900

    Dashではなく、Chart.jsを利用することにした。その他諸々変更したが忘れた…。

--- apps/static/bodyCompositionVisualization.js
+++ apps/static/bodyCompositionVisualization.js
@@ -1,101 +1,199 @@
-/** 
- * 全体のデータを保持するオブジェクト
- * @type {Object} 
- */
-let allData = {};
-
-/**
- * グラフのインスタンスを保持する変数
- * @type {Chart|null}
- */
+let dailyData = {
+  date: [],
+  weight: [],
+  bodyFat: []
+};
+let monthlyData = {
+  date: [],
+  weight: [],
+  bodyFat: [],
+  weightChangeRate: []
+};
 let chartInstance = null;
 
-/**
- * グラフを作成または更新します。
- * @param {string[]} labels - グラフのX軸のラベル（日付）
- * @param {number[]} weightData - 体重のデータ配列
- * @param {number[]} bodyFatData - 体脂肪率のデータ配列
- */
-function createOrUpdateGraph(labels, weightData, bodyFatData) {
-  const ctx = document.getElementById("bodyCompositionGraph").getContext("2d");
+async function initializePage() {
+  await fetchData();
+  createGraph(dailyData.date, dailyData.weight, dailyData.bodyFat);
+};
 
-  if (chartInstance) {
-    chartInstance.data.labels = labels;
-    chartInstance.data.datasets[0].data = weightData;
-    chartInstance.data.datasets[1].data = bodyFatData;
-    chartInstance.update();
-  } else {
-    chartInstance = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: "体重",
-            data: weightData,
-            borderColor: "rgba(255, 99, 132, 1)",
-            yAxisID: "y",
-            spanGaps: true
-          },
-          {
-            label: "体脂肪率",
-            data: bodyFatData,
-            borderColor: "rgba(54, 162, 235, 1)",
-            yAxisID: "y1",
-            spanGaps: true
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        interaction: {
-          mode: "index",
-          intersect: false,
+async function fetchData() {
+  const response = await fetch("/get_body_composition_data");
+  const body_composition_data = await response.json();
+
+  const dailyDataSet = body_composition_data[0];
+  dailyData.date = dailyDataSet.map(item => item.date);
+  dailyData.weight = dailyDataSet.map(item => item.weight);
+  dailyData.bodyFat = dailyDataSet.map(item => item.body_fat);
+
+  const monthlyDataSet = body_composition_data[1];
+  monthlyData.date = monthlyDataSet.map(item => item.date);
+  monthlyData.weight = monthlyDataSet.map(item => item.weight);
+  monthlyData.bodyFat = monthlyDataSet.map(item => item.body_fat);
+  monthlyData.weightChangeRate = monthlyDataSet.map(item => item.weight_change_rate);
+}
+
+function createGraph(date, weight, bodyFat) {
+  const ctx = document.getElementById("bodyCompositionGraph").getContext("2d");
+  chartInstance = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: date,
+      datasets: [
+        {
+          label: "体重",
+          data: weight,
+          yAxisID: "y",
+          spanGaps: true,
+          pointRadius: 0,
+          pointHoverRadius: 0
         },
-        stacked: false,
-        plugins: {
+        {
+          label: "体脂肪率",
+          data: bodyFat,
+          yAxisID: "y1",
+          spanGaps: true,
+          pointRadius: 0,
+          pointHoverRadius: 0
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      interaction: {
+        mode: "index",
+        intersect: false,
+      },
+      stacked: false,
+      plugins: {
+        title: {
+          display: true,
+          text: "体重と体脂肪率の推移",
+        },
+      },
+      scales: {
+        y: {
+          type: "linear",
+          display: true,
+          position: "left",
           title: {
             display: true,
-            text: "体重と体脂肪率の推移",
+            text: "体重 (kg)",
           },
         },
-        scales: {
-          y: {
-            type: "linear",
+        y1: {
+          type: "linear",
+          display: true,
+          position: "right",
+          title: {
             display: true,
-            position: "left",
-            title: {
-              display: true,
-              text: "体重 (kg)",
-            },
+            text: "体脂肪率 (%)",
           },
-          y1: {
-            type: "linear",
-            display: true,
-            position: "right",
-            title: {
-              display: true,
-              text: "体脂肪率 (%)",
-            },
-            grid: {
-              drawOnChartArea: false,
-            },
+          grid: {
+            drawOnChartArea: false,
           },
         },
       },
-    });
+    },
+  });
+}
+
+window.onload = initializePage();
+
+function updateGraph(date, weight, bodyFat) {
+  chartInstance.data.labels = date;
+  chartInstance.data.datasets[0].data = weight;
+  chartInstance.data.datasets[1].data = bodyFat;
+  chartInstance.update();
+}
+
+//ここから下は作成していく。
+function filterDataByDuration(data, days) {
+  const cutoffDate = new Date();
+  cutoffDate.setDate(cutoffDate.getDate() - days);
+
+  return {
+    date: data.date.filter(d => new Date(d) >= cutoffDate),
+    weight: data.weight.slice(-days),
+    bodyFat: data.bodyFat.slice(-days)
+  };
+}
+
+function updateDataDisplay(granularity) {
+  if (granularity === "daily") {
+    updateGraph(dailyData.date, dailyData.weight, dailyData.bodyFat);
+  } else if (granularity === "monthly") {
+    updateGraph(monthlyData.date, monthlyData.weight, monthlyData.bodyFat);
   }
 }
 
-/**
- * 表を更新します。
- * @param {string[]} dates - 日付の配列
- * @param {number[]} weightData - 体重のデータ配列
- * @param {number[]} bodyFatData - 体脂肪率のデータ配列
- */
-function updateTable(dates, weightData, bodyFatData) {
+
+
+// 表示粒度を変更した場合の処理
+document.getElementById("granularity-dropdown").addEventListener("change", function () {
+  const granularity = this.value;
+  let options = [
+    { value: "7", label: "1週間" },
+    { value: "30", label: "1か月" },
+    { value: "90", label: "3か月" },
+    { value: "180", label: "半年" },
+    { value: "365", label: "1年" },
+    { value: "1095", label: "3年" }
+  ];
+
+  if (granularity === "monthly") {
+    options = options.filter(opt => opt.value !== "7");
+  }
+
+  let defaultSelectedValue = granularity === "monthly" ? "365" : "30";
+
+  const durationDropdown = document.getElementById("duration-dropdown");
+  durationDropdown.innerHTML = options.map(opt => {
+    let selected = opt.value === defaultSelectedValue ? ' selected' : '';
+    return `<option value="${opt.value}"${selected}>${opt.label}</option>`;
+  }).join('');
+
+  const durationValue = parseInt(durationDropdown.value);
+
+  const filteredData = filterDataByDuration(allData, durationValue);
+  updateDataDisplay(granularity, filteredData);
+});
+
+// 表示期間を変更した場合の処理
+document.getElementById("duration-dropdown").addEventListener("change", function () {
+  const granularityValue = document.getElementById("granularity-dropdown").value;
+  const durationValue = parseInt(this.value);
+  const targetData = granularityValue === "daily" ? dailyData : monthlyData;
+
+  const filteredData = filterDataByDuration(targetData, durationValue);
+
+  console.log(filteredData)
+  updateDataDisplay(granularityValue, filteredData);
+});
+
+
+
+
+/*
+function updateTable(dates, weightData, bodyFatData, weightChangeRate, granularity) {
   const tableContainer = document.getElementById("body-composition-table");
-  let tableHtml = '<thead><tr><th>日付</th><th>体重 (kg)</th><th>体脂肪率 (%)</th></tr></thead><tbody>';
+
+  let weightHeader = granularity === "monthly" ? "平均体重 (kg)" : "体重 (kg)";
+  let bodyFatHeader = granularity === "monthly" ? "平均体脂肪率 (%)" : "体脂肪率 (%)";
+
+  let tableHtml = `
+  <thead>
+  <tr>
+  <th>日付</th>
+  <th>${weightHeader}</th>
+  <th>${bodyFatHeader}</th>
+  `;
+
+  // 月別の表示の場合だけ、体重変化率を追加
+  if (granularity === "monthly") {
+    tableHtml += `<th>体重変化率 (%)</th>`;
+  }
+
+  tableHtml += `</tr></thead><tbody>`;
 
   const indices = Array.from({ length: dates.length }, (_, i) => i);
   indices.sort((a, b) => {
@@ -105,81 +203,84 @@ function updateTable(dates, weightData, bodyFatData) {
   });
 
   for (const i of indices) {
-    tableHtml += `<tr><td>${dates[i]}</td><td>${weightData[i]}</td><td>${bodyFatData[i] || '-'}</td></tr>`;
+    tableHtml += `<tr>
+    <td>${dates[i]}</td>
+    <td>${weightData[i].toFixed(2)}</td>
+    <td>${bodyFatData[i] ? bodyFatData[i].toFixed(2) : "N/A"}</td>`;
+    if (granularity === "monthly" && weightChangeRate[i]) {
+      tableHtml += `<td>${weightChangeRate[i].toFixed(2)}</td>`;
+    }
+    tableHtml += `</tr>`;
   }
 
-  tableHtml += '</tbody>';
+  tableHtml += `</tbody>`;
   tableContainer.innerHTML = tableHtml;
 }
 
-/**
- * 指定した期間に基づいてデータをフィルタリングします。
- * @deprecated この関数は古い方式でのフィルタリングのため、代わりにfilterDataByDaysを使用してください。
- * @param {Object} data - フィルタリングする前のデータ
- * @param {string} duration - 表示期間（"all"か日数）
- * @returns {Object} フィルタリングされたデータ
- */
-function filterDataByDuration(data, duration) {
-  let filteredData;
-
-  if (duration === "all") {
-    filteredData = data;
-  } else {
-    const durationInt = parseInt(duration, 10);
-    filteredData = {
-      dates: data.dates.slice(-durationInt),
-      weight_data: data.weight_data.slice(-durationInt),
-      body_fat_data: data.body_fat_data.slice(-durationInt)
-    };
-  }
-
-  return filteredData;  // ここでは日付をソートせず、そのまま返す
-}
-
-/**
- * 指定した日数に基づいてデータをフィルタリングします。
- * @param {Object} data - フィルタリングする前のデータ
- * @param {number|string} days - フィルタリングする日数（"all"か日数）
- * @returns {Object} フィルタリングされたデータ
- */
 function filterDataByDays(data, days) {
   const now = new Date();
-  const cutoff = new Date();
-  cutoff.setDate(now.getDate() - days);
-
-  const filteredData = {
-    weight_data: [],
-    body_fat_data: [],
-    dates: []
-  };
-
-  for (let i = 0; i < data.dates.length; i++) {
-    const date = new Date(data.dates[i]);
-    if (days === "all" || date >= cutoff) {
-      filteredData.weight_data.push(data.weight_data[i]);
-      filteredData.body_fat_data.push(data.body_fat_data[i]);
-      filteredData.dates.push(data.dates[i]);
-    }
-  }
-  return filteredData;
+  const cutoffDate = new Date();
+  cutoffDate.setDate(now.getDate() - days);
+  return data.filter(item => new Date(item.date) > cutoffDate);
 }
 
-// ドロップダウンメニューの変更を監視し、データをフィルタリングしてグラフと表を更新
-document.getElementById("duration-dropdown").addEventListener("change", function () {
-  const days = this.value;
-  const filteredData = filterDataByDays(allData, days === "all" ? days : parseInt(days));
-  createOrUpdateGraph(filteredData.dates, filteredData.weight_data, filteredData.body_fat_data);
-  updateTable(filteredData.dates, filteredData.weight_data, filteredData.body_fat_data);
-});
+function average(arr) {
+  if (arr.length === 0) return null;
+  const sum = arr.reduce((acc, val) => acc + val, 0);
+  return parseFloat((sum / arr.length).toFixed(2));
+}
 
-// サーバーからデータを取得し、グラフと表を初期化
-fetch("/get_body_composition_data")
-  .then((response) => response.json())
-  .then((data) => {
-    allData = data;
-    const initialData = filterDataByDays(allData, 30);
+function processMonthlyData(data) {
+  const monthlyData = {
+    dates: [],
+    weight_data: [],
+    body_fat_data: [],
+    weight_change_rate: []  // 体重変化率のデータを保持する配列
+  };
+  
+  let tempWeights = [];
+  let tempBodyFats = [];
+  let currentMonth = new Date(data.dates[0]).getMonth();
+  let currentYear = new Date(data.dates[0]).getFullYear();
+  
+  for (let i = 0; i < data.dates.length; i++) {
+    const date = new Date(data.dates[i]);
+    const month = date.getMonth();
+  const year = date.getFullYear();
+  
+  if (month !== currentMonth || i === data.dates.length - 1) {
+    if (i === data.dates.length - 1 && month === currentMonth) {
+      tempWeights.push(data.weight_data[i]);
+      tempBodyFats.push(data.body_fat_data[i]);
+    }
+    
+    monthlyData.dates.push(`${currentYear}-${String(currentMonth + 1).padStart(2, '0')}`);
+    monthlyData.weight_data.push(average(tempWeights));
+    monthlyData.body_fat_data.push(average(tempBodyFats));
+    
+    // 体重変化率の計算
+    if (monthlyData.weight_data.length > 1) {
+      const prevWeight = monthlyData.weight_data[monthlyData.weight_data.length - 2];
+      const currentWeight = average(tempWeights);
+      const changeRate = ((currentWeight - prevWeight) / prevWeight) * 100;
+      monthlyData.weight_change_rate.push(parseFloat(changeRate.toFixed(2))); // 小数点第2位まで四捨五入
+    } else {
+      // 最初のデータポイントでは変化率が計算できないのでnullを挿入
+      monthlyData.weight_change_rate.push(null);
+    }
+    
+    tempWeights = [];
+    tempBodyFats = [];
+    currentMonth = month;
+    currentYear = year;
+  }
 
-    // グラフとテーブルを更新
-    createOrUpdateGraph(initialData.dates, initialData.weight_data, initialData.body_fat_data);
-    updateTable(initialData.dates, initialData.weight_data, initialData.body_fat_data);
-  });
+  if (i !== data.dates.length - 1 || month !== currentMonth) {
+    tempWeights.push(data.weight_data[i]);
+    tempBodyFats.push(data.body_fat_data[i]);
+  }
+}
+ 
+return monthlyData;
+}
+*/
--- apps/views.py
+++ apps/views.py
@@ -1,6 +1,8 @@
 import random
 from datetime import date, datetime, timedelta
+import json
 
+import pandas as pd
 from flask import Blueprint, flash, jsonify, redirect, render_template, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
@@ -144,60 +146,99 @@ def logout():
 def fetch_body_composition_data():
     if current_user.is_authenticated:
         user_id = current_user.id
-        weight_data, body_fat_data, dates = get_body_composition_data(user_id)
+        body_composition_objects = get_body_compositions(user_id)
+        body_composition_object_lists = convert_objects_to_object_lists(
+            body_composition_objects
+        )
     else:
-        weight_data, body_fat_data, dates = generate_dummy_data()
+        body_composition_object_lists = generate_dummy_data_object_list()
+
+    body_composition_df = pd.DataFrame(body_composition_object_lists)
+    body_composition_df["date"] = pd.to_datetime(body_composition_df["date"])
+    body_composition_df.set_index("date", inplace=True)
+    monthly_averages_and_weight_change_rates_df = (
+        compute_monthly_averages_and_weight_change_rates(body_composition_df)
+    )
+    monthly_averages_and_weight_change_rates_json = convert_dataframe_to_json(
+        monthly_averages_and_weight_change_rates_df
+    )
 
     return jsonify(
-        {"weight_data": weight_data, "body_fat_data": body_fat_data, "dates": dates}
+        body_composition_object_lists, monthly_averages_and_weight_change_rates_json
     )
 
 
-def get_body_composition_data(user_id):
-    weight_data = []
-    body_fat_data = []
-    dates = []
-
-    body_compositions = (
+def get_body_compositions(user_id):
+    body_composition_objects = (
         BodyComposition.query.filter_by(user_id=user_id)
         .order_by(BodyComposition.date.asc())
         .all()
     )
-
-    for body_composition in body_compositions:
-        weight_data.append(body_composition.weight)
-        body_fat_data.append(body_composition.body_fat)
-        dates.append(body_composition.date.strftime("%Y-%m-%d"))
-
-    return weight_data, body_fat_data, dates
+    return body_composition_objects
 
 
-def generate_dummy_data():
-    duration = 365 * 1.5
+def convert_objects_to_object_lists(body_composition_objects):
+    body_composition_object_lists = [
+        {
+            "date": bco.date.strftime("%Y-%m-%d"),
+            "weight": bco.weight,
+            "body_fat": bco.body_fat,
+        }
+        for bco in body_composition_objects
+    ]
+    return body_composition_object_lists
+
+
+def compute_monthly_averages_and_weight_change_rates(body_composition_df):
+    monthly_averages_and_weight_change_rates_df = (
+        body_composition_df.resample("M").mean().round(2)
+    )
+
+    monthly_averages_and_weight_change_rates_df["weight_change_rate"] = (
+        (
+            monthly_averages_and_weight_change_rates_df["weight"]
+            - monthly_averages_and_weight_change_rates_df["weight"].shift(1)
+        )
+        / monthly_averages_and_weight_change_rates_df["weight"].shift(1)
+        * 100
+    ).round(2)
+
+    monthly_averages_and_weight_change_rates_df.reset_index(inplace=True)
+    monthly_averages_and_weight_change_rates_df[
+        "date"
+    ] = monthly_averages_and_weight_change_rates_df["date"].dt.strftime("%Y-%m")
+
+    return monthly_averages_and_weight_change_rates_df
+
+
+def convert_dataframe_to_json(df):
+    json_data = df.to_json(orient="records")
+    return json.loads(json_data)
+
+
+def generate_dummy_data_object_list():
+    duration = 365 * 3
     today = datetime.now()
-    dummy_date = today - timedelta(days=int(duration))
+    date = today - timedelta(days=int(duration))
 
-    initial_weight = round(random.uniform(120, 150), 2)
-    initial_body_fat = round(random.uniform(30, 40), 2)
+    weight = round(random.uniform(90, 100), 2)
+    body_fat = round(random.uniform(25, 30), 2)
 
-    current_weight = initial_weight
-    current_body_fat = initial_body_fat
+    body_composition_object_lists = []
+    while date <= today:
+        weight_variation = round(random.uniform(-0.4, 0.36), 2)
+        weight = round(max(weight + weight_variation, 50), 2)
 
-    dates = []
-    weight_data = []
-    body_fat_data = []
-    while dummy_date <= today:
-        dates.append(dummy_date.strftime("%Y-%m-%d"))
-        dummy_date += timedelta(days=1)
+        body_fat_variation = round(random.uniform(-0.2, 0.18), 2)
+        body_fat = round(max(body_fat + body_fat_variation, 5), 2)
 
-        weight_variation = round(random.uniform(-0.5, 0.3), 2)
-        updated_weight = round(max(current_weight + weight_variation, 50), 2)
-        weight_data.append(updated_weight)
-        current_weight = updated_weight
+        body_composition_object = {
+            "date": date.strftime("%Y-%m-%d"),
+            "weight": weight,
+            "body_fat": body_fat,
+        }
+        body_composition_object_lists.append(body_composition_object)
 
-        body_fat_variation = round(random.uniform(-0.4, 0.32), 2)
-        updated_body_fat = round(max(current_body_fat + body_fat_variation, 5), 2)
-        body_fat_data.append(updated_body_fat)
-        current_body_fat = updated_body_fat
+        date += timedelta(days=1)
 
-    return weight_data, body_fat_data, dates
+    return body_composition_object_lists
