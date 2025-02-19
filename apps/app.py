4b69bd69439d03115b2d7abcf3e49e41e52d393c
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2025-02-19 08:54:23 +0000

    Update: Add type hint.

--- apps/app.py
+++ apps/app.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from flask import Flask, g, render_template
 from flask_login import LoginManager, current_user
 
@@ -5,7 +7,7 @@ from .config import config
 from .models import User, db
 
 
-def create_app(config_key):
+def create_app(config_key: str) -> Flask:
     app = Flask(__name__)
     app.config.from_object(config[config_key])
 
@@ -18,7 +20,7 @@ def create_app(config_key):
     login_manager.login_view = "main.signin"
 
     @login_manager.user_loader
-    def load_user(user_id):
+    def load_user(user_id: str) -> User:
         return User.query.get(int(user_id))
 
     login_manager.init_app(app)
@@ -31,15 +33,15 @@ def create_app(config_key):
     app.register_error_handler(500, internal_server_error)
 
     @app.before_request
-    def before_request():
+    def before_request() -> None:
         g.user = current_user
 
     return app
 
 
-def page_not_found(e):
+def page_not_found(e: Any) -> tuple[str, int]:
     return render_template("404.html"), 404
 
 
-def internal_server_error(e):
+def internal_server_error(e: Any) -> tuple[str, int]:
     return render_template("500.html"), 500
