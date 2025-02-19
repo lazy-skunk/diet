a5ecffc8e25ee111fb1052e574ebdd9e2c6b25fa
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2025-02-19 09:47:59 +0000

    Refactor: Minor modification.

--- apps/forms.py
+++ apps/forms.py
@@ -1,14 +1,7 @@
 from datetime import date
 
 from flask_wtf import FlaskForm
-from wtforms import (
-    DateField,
-    FloatField,
-    PasswordField,
-    StringField,
-    SubmitField,
-    validators,
-)
+from wtforms import DateField, PasswordField, StringField, SubmitField
 from wtforms.fields import FloatField
 from wtforms.validators import (
     DataRequired,
@@ -29,14 +22,18 @@ class CustomBodyFatFloatField(FloatField):
     def process_formdata(self, valuelist: list) -> None:
         if valuelist:
             try:
-                self.data = float(valuelist[0])
+                self.data: float | None = float(valuelist[0])
             except ValueError:
                 self.data = None
-                raise ValidationError("体脂肪率には正の整数、または正の浮動小数点数を入力してください。")
+                raise ValidationError(
+                    "体脂肪率には正の整数、または正の浮動小数点数を入力してください。"
+                )
 
 
 class BaseLogBodyCompositionForm(FlaskForm):
-    def validate_not_future_date(form: "BaseLogBodyCompositionForm", field: DateField) -> None:
+    def validate_not_future_date(
+        form: "BaseLogBodyCompositionForm", field: DateField
+    ) -> None:
         if field.data > date.today():
             raise ValidationError("今日以前の日付を選択してください。")
 
@@ -44,7 +41,10 @@ class BaseLogBodyCompositionForm(FlaskForm):
 
     date = DateField(
         label="日付",
-        validators=[DataRequired(message="日付を入力してください。"), validate_not_future_date],
+        validators=[
+            DataRequired(message="日付を入力してください。"),
+            validate_not_future_date,
+        ],
         default=date.today,
         render_kw={"max": today},
     )
@@ -53,7 +53,11 @@ class BaseLogBodyCompositionForm(FlaskForm):
         label="体重 (kg)",
         validators=[
             DataRequired(message="体重を入力してください。"),
-            NumberRange(min=0.1, max=999.9, message="体重は 0.1 から 999.9 の間で入力してください。"),
+            NumberRange(
+                min=0.1,
+                max=999.9,
+                message="体重は 0.1 から 999.9 の間で入力してください。",
+            ),
         ],
     )
 
@@ -61,7 +65,11 @@ class BaseLogBodyCompositionForm(FlaskForm):
         label="体脂肪率 (%)",
         validators=[
             Optional(),
-            NumberRange(min=0, max=99.9, message="体脂肪率は 0 から 99.9 の間で入力してください。"),
+            NumberRange(
+                min=0,
+                max=99.9,
+                message="体脂肪率は 0 から 99.9 の間で入力してください。",
+            ),
         ],
     )
 
@@ -72,19 +80,28 @@ class LogBodyCompositionForm(BaseLogBodyCompositionForm):
 
 class SignupForm(BaseLogBodyCompositionForm):
     username = StringField(
-        label="ユーザー名", validators=[DataRequired(message="ユーザー名を入力してください。")]
+        label="ユーザー名",
+        validators=[DataRequired(message="ユーザー名を入力してください。")],
     )
     email = StringField(
-        label="Eメール", validators=[DataRequired(message="Eメールアドレスを入力してください。"), Email()]
+        label="Eメール",
+        validators=[
+            DataRequired(message="Eメールアドレスを入力してください。"),
+            Email(),
+        ],
     )
     password = PasswordField(
-        label="パスワード", validators=[DataRequired(message="パスワードを入力してください。")]
+        label="パスワード",
+        validators=[DataRequired(message="パスワードを入力してください。")],
     )
     confirm_password = PasswordField(
         label="パスワード (確認用)",
         validators=[
             DataRequired(message="パスワード (確認用) を入力してください。"),
-            EqualTo("password", message="パスワードとパスワード (確認用) には同一の値を入力してください。"),
+            EqualTo(
+                "password",
+                message="パスワードとパスワード (確認用) には同一の値を入力してください。",
+            ),
         ],
     )
     submit = SubmitField("登録")
@@ -92,9 +109,11 @@ class SignupForm(BaseLogBodyCompositionForm):
 
 class SigninForm(FlaskForm):
     username = StringField(
-        label="ユーザー名", validators=[DataRequired(message="ユーザー名を入力してください。")]
+        label="ユーザー名",
+        validators=[DataRequired(message="ユーザー名を入力してください。")],
     )
     password = PasswordField(
-        label="パスワード", validators=[DataRequired(message="パスワードを入力してください。")]
+        label="パスワード",
+        validators=[DataRequired(message="パスワードを入力してください。")],
     )
     submit = SubmitField("ログイン")
