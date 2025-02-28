4376428331f8f53463b0a661e983047e617a8cb6
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2025-02-28 13:26:55 +0000

    Add: Add service class.

--- apps/body_composition/views.py
+++ apps/body_composition/views.py
@@ -4,14 +4,26 @@ from datetime import date, datetime, timedelta
 from typing import Any
 
 import pandas as pd
-from flask import Blueprint, flash, jsonify, redirect, render_template, url_for
+from flask import (
+    Blueprint,
+    flash,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
 from flask_login import current_user, login_required
 from pandas import DataFrame
+from sqlalchemy.exc import SQLAlchemyError
 from werkzeug.wrappers import Response
 
+from apps.body_composition.services.body_composition_service import (
+    BodyCompositionService,
+)
 from apps.extensions import sql_alchemy
 
-from .forms import LogBodyCompositionForm
+from .forms import RecordBodyCompositionForm
 from .models import BodyComposition
 
 blueprint = Blueprint(
@@ -23,74 +35,41 @@ blueprint = Blueprint(
 )
 
 
-# TODO: 循環的複雑度が高い。
-# TODO: views が MVC でいう controller だとしたら肥大化しすぎている。
-@blueprint.route("/log_body_composition", methods=["GET", "POST"])
+@blueprint.route("/record_body_composition", methods=["GET", "POST"])
 @login_required
-def log_body_composition() -> str | Response:
+def record_body_composition() -> str | Response:
     user_id = current_user.id
-    form = LogBodyCompositionForm()
+    form = RecordBodyCompositionForm()
 
-    # フォーム入力時処理
+    if request.method == "GET":
+        form_data = BodyCompositionService.init_form_data(user_id)
+        form.weight.data = form_data["weight"]
+        form.body_fat.data = form_data["body_fat"]
+        return render_template(
+            "body_composition/record_body_composition.html", form=form
+        )
+
     if form.validate_on_submit():
-        log_date = form.date.data
+        input_date = form.date.data
         weight = form.weight.data
         body_fat = form.body_fat.data
 
-        specified_date_log = BodyComposition.query.filter_by(
-            date=form.date.data, user_id=user_id
-        ).first()
-
-        if specified_date_log:
-            specified_date_log.weight = weight
-            specified_date_log.body_fat = body_fat
-        else:
-            try:
-                log = BodyComposition(
-                    date=log_date,
-                    weight=weight,
-                    body_fat=body_fat,
-                    user_id=user_id,
-                )
-                sql_alchemy.session.add(log)
-            except Exception as e:
-                flash(f"{str(e)}", "danger")
-                return render_template(
-                    "body_composition/log_body_composition.html",
-                    form=form,
-                )
-
         try:
-            sql_alchemy.session.commit()
+            BodyCompositionService.upsert(
+                user_id, input_date, weight, body_fat
+            )
+            flash("Body composition data saved successfully.", "success")
             return redirect(url_for("main.index"))
-        except Exception as e:
-            sql_alchemy.session.rollback()
-            flash(f"予期せぬエラーが発生しました。: {str(e)}", "danger")
-            return render_template(
-                "body_composition/log_body_composition.html", form=form
+        except (ValueError, TypeError) as e:
+            flash(str(e), "danger")
+        except SQLAlchemyError:
+            flash(
+                "Body composition record failed. Please try again later.",
+                "danger",
             )
 
-    # フォーム初期値設定
-    today = date.today()
-    todays_log = BodyComposition.query.filter_by(
-        date=today, user_id=user_id
-    ).first()
-
-    if todays_log:
-        form.weight.data = todays_log.weight
-        form.body_fat.data = todays_log.body_fat
-    else:
-        last_registered_log = (
-            BodyComposition.query.filter_by(user_id=user_id)
-            .order_by(BodyComposition.date.desc())
-            .first()
-        )
-        if last_registered_log:
-            form.weight.data = last_registered_log.weight
-            form.body_fat.data = last_registered_log.body_fat
-
     return render_template(
-        "body_composition/log_body_composition.html", form=form
+        "body_composition/record_body_composition.html", form=form
     )
 
 
@@ -153,13 +132,13 @@ def fetch_body_composition_data() -> Response:
     def _generate_dummy_data_object_list() -> list[dict[str, Any]]:
         duration = 365 * 3
         today = datetime.now()
-        date = today - timedelta(days=int(duration))
+        past_date = today - timedelta(days=int(duration))
 
         weight = round(random.uniform(90, 100), 2)
         body_fat = round(random.uniform(25, 30), 2)
 
         body_composition_object_lists = []
-        while date <= today:
+        while past_date <= today:
             weight_variation = round(random.uniform(-0.4, 0.36), 2)
             weight = round(max(weight + weight_variation, 50), 2)
 
@@ -167,13 +146,13 @@ def fetch_body_composition_data() -> Response:
             body_fat = round(max(body_fat + body_fat_variation, 5), 2)
 
             body_composition_object = {
-                "date": date.strftime("%Y-%m-%d"),
+                "date": past_date.strftime("%Y-%m-%d"),
                 "weight": weight,
                 "body_fat": body_fat,
             }
             body_composition_object_lists.append(body_composition_object)
 
-            date += timedelta(days=1)
+            past_date += timedelta(days=1)
 
         return body_composition_object_lists
 
