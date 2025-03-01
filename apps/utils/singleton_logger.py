2f7c42facc35e07fdd14bb71fce9ee326bb7b2e8
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2025-03-01 02:26:45 +0000

    Refactor: Minor modification.

--- apps/utils/singleton_logger.py
+++ apps/utils/singleton_logger.py
@@ -3,7 +3,7 @@ import os
 from logging import Formatter, Logger, StreamHandler
 from logging.handlers import RotatingFileHandler
 
-from apps.utils.directory_manager import DirectoryManager
+from apps.utils.directory_util import DirectoryUtil
 
 _DEFAULT_LOG_LEVEL = logging.DEBUG
 _DEFAULT_LOG_PATH = "log/app.log"
@@ -18,14 +18,10 @@ class SingletonLogger:
     def get_logger(cls) -> Logger:
         cls._initialize()
 
-        cls._ensure_logger_exists()
-
-        return cls._logger
-
-    @classmethod
-    def _ensure_logger_exists(cls) -> None:
         if cls._logger is None:  # pragma: no cover
             raise RuntimeError("Logger has not been initialized.")
+
+        return cls._logger
 
     @classmethod
     def _initialize(cls) -> None:
@@ -36,7 +32,7 @@ class SingletonLogger:
             cls._logger.setLevel(log_level)
 
             log_path = os.getenv("LOG_PATH", _DEFAULT_LOG_PATH)
-            DirectoryManager.ensure_directory(log_path)
+            DirectoryUtil.ensure_directory(log_path)
 
             formatter = Formatter(
                 "%(asctime)s - %(levelname)s - %(message)s",
@@ -49,7 +45,8 @@ class SingletonLogger:
     def _add_stream_handler(
         cls, log_level: str | int, formatter: Formatter
     ) -> None:
-        cls._ensure_logger_exists()
+        if cls._logger is None:  # pragma: no cover
+            raise RuntimeError("Logger has not been initialized.")
 
         stream_handler = StreamHandler()
         stream_handler.setLevel(log_level)
@@ -61,7 +58,8 @@ class SingletonLogger:
     def _add_rotating_file_handler(
         cls, log_level: str | int, log_path: str, formatter: Formatter
     ) -> None:
-        cls._ensure_logger_exists()
+        if cls._logger is None:  # pragma: no cover
+            raise RuntimeError("Logger has not been initialized.")
 
         log_size = int(os.getenv("LOG_SIZE", _DEFAULT_LOG_SIZE))
         log_backup = int(os.getenv("LOG_BACKUP", _DEFAULT_LOG_BACKUP))
