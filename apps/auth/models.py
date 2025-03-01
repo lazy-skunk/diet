2cd98fb27ca66837df6c1fb247881ea5a121ef49
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2025-03-01 07:15:07 +0000

    Refactor: Minor modification.

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -2,10 +2,10 @@ from flask_login import UserMixin
 from sqlalchemy.orm import validates
 
 from apps.extensions import sql_alchemy
-from apps.utils.singleton_logger import SingletonLogger
-from apps.utils.validation_helper import ValidationHelper
+from apps.utils.custom_logger import CustomLogger
+from apps.utils.validation_util import ValidationUtil
 
-_logger = SingletonLogger.get_logger()
+_logger = CustomLogger.get_logger()
 
 _EMAIL_REGEXP = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
 _MAX_EMAIL_LENGTH = 254
@@ -25,25 +25,25 @@ class User(sql_alchemy.Model, UserMixin):  # type: ignore
 
     @validates("username")
     def validate_username(self, key: str, username: str) -> str:
-        _logger.debug(f"Validating {key}: {key}={username}")
+        _logger.debug(f"Start: {key}={username}")
 
-        ValidationHelper.validate_not_none(key, username)
+        ValidationUtil.validate_not_none(key, username)
         username = username.strip()
-        ValidationHelper.validate_not_empty(key, username)
+        ValidationUtil.validate_not_empty(key, username)
 
-        _logger.debug(f"Validation passed: {key}={username}")
+        _logger.debug(f"End: {key}={username}")
         return username
 
     @validates("email")
     def validate_email(self, key: str, email: str) -> str:
-        _logger.debug(f"Validating {key}: {key}={email}")
+        _logger.debug(f"Start: {key}={email}")
 
-        ValidationHelper.validate_not_none(key, email)
+        ValidationUtil.validate_not_none(key, email)
         email = email.strip().lower()
-        ValidationHelper.validate_not_empty(key, email)
-        ValidationHelper.validate_by_regexp(key, email, _EMAIL_REGEXP)
-        ValidationHelper.validate_by_max_length(key, email, _MAX_EMAIL_LENGTH)
-        ValidationHelper.validate_unique(User, key, email)
+        ValidationUtil.validate_not_empty(key, email)
+        ValidationUtil.validate_by_regexp(key, email, _EMAIL_REGEXP)
+        ValidationUtil.validate_by_max_length(key, email, _MAX_EMAIL_LENGTH)
+        ValidationUtil.validate_unique(User, key, email)
 
-        _logger.debug(f"Validation passed: {key}={email}")
+        _logger.debug(f"End: {key}={email}")
         return email
